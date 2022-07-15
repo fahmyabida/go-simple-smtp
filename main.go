2f633abff9ae329b2033f05c238c88eb8dfa9fb9
fahmyabida <fahmyabida@gmail.com>
2022-07-15 21:03:57 +0700

    add

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
-	sendEmail()
+	// sendEmail()
+	e := echo.New()
+
+	e.POST("/callback", func(c echo.Context) error {
+		var iface interface{}
+		c.Bind(&iface)
+		asByteJSON, _ := json.Marshal(iface)
+		fmt.Println("masuk : ", string(asByteJSON))
+		return c.JSON(200, map[string]interface{}{
+			"message": "ok",
+		})
+	})
+
+	e.Start(":8080")
 }
